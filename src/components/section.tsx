d22887a5a5c16c28161f0e489f2ac0a2bb1b0acf
Sarah <sarah.theoulle@etu.umontpellier.fr>
2024-12-05 23:51:44 +0100

    feat(bg) : add images for background
    add image
    
    add globe and trace

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,8 +5,11 @@ import Popup from './popup';
 import QuizForm from './quiz-form';
 
 export interface SectionProps {
-    title: string;
-    content: string;
+   title: string;
+   content: string;
+   image: string;
+   trait: string;
+   globe: string;
     quiz?: {
         question: string;
         answers: { title: string, isCorrect: boolean }[];
@@ -14,19 +17,31 @@ export interface SectionProps {
     }
 }
 
-export default function Section({ title, content, quiz }: SectionProps) {
+export default function Section({ title, content, image, trait, globe, quiz }: SectionProps) {
+   console.log(image);
+   console.log(trait);
     const [showQuestions, setShowQuestions] = useState(false);
 
-    return (
-        <motion.div
-            className="section"
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.8 }}
-        >
-            <h2>{title}</h2>
-            <p>{content}</p>
+   return (
+      <motion.div
+         className="relative h-screen flex items-center justify-center text-white"
+         style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+         }}
+         initial={{ opacity: 0, y: 50 }}
+         whileInView={{ opacity: 1, y: 0 }}
+         viewport={{ once: true }}
+         transition={{ duration: 0.8 }}
+      >
+         {' '}
+         <img src={globe} alt="Globe" className="absolute top-0 right-0 h-auto w-[20%] object-contain z-10" />
+         <img src={trait} alt="Trait décoratif" className="absolute top-0 left-0 h-full w-auto object-cover z-10" />
+         <div className="bg-black bg-opacity-50 p-6 rounded-lg text-center max-w-2xl z-20">
+            <h2 className="text-4xl font-bold mb-4">{title}</h2>
+            <p className="text-lg">{content}</p>
+         </div>
             {
                 quiz && (
                     <>
@@ -45,6 +60,6 @@ export default function Section({ title, content, quiz }: SectionProps) {
                     </>
                 )
             }
-        </motion.div>
-    );
+      </motion.div>
+   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,40 +1,102 @@
 import { Element } from 'react-scroll';
-import Section from "@/components/section.tsx";
+import Section from '@/components/section.tsx';
 
-export default function PageHome () {
-    const sections = [
-        { id: 'etape1', title: 'Étape 1: Golfe de Gascogne', content: 'Les skippers affrontent des vagues et des vents violents dans cette première étape.' },
-        { id: 'etape2', title: 'Étape 2: Anticyclone des Açores', content: 'Les marins traversent une zone de calme, où il n’y a pas de vent.' },
-        { id: 'etape3', title: 'Étape 3: Pot au Noir', content: 'Un phénomène météorologique chaud et humide qui affecte la visibilité et la navigation.' },
-        { id: 'etape4', title: 'Étape 4: Anticyclone de Saint-Hélène', content: 'Zone de haute pression avec de beaux temps, mais difficile à contourner.' },
-        { id: 'etape5', title: 'Étape 5: Cap de Bonne-Espérance', content: 'Les marins traversent l’Atlantique Sud et se dirigent vers les mers australes.' },
-        { id: 'etape6', title: 'Étape 6: Cap Leeuwin', content: 'Les skippers passent près de l’Australie, entrée dans l’océan Pacifique.' },
-        { id: 'etape7', title: 'Étape 7: Point Nemo', content: 'Le point le plus éloigné de toute terre émergée, au cœur du Pacifique Sud.', 
-            quiz: {
-                "question": "Quel est l'un des défis majeurs pour les skippers lors du passage près du point Nemo pendant le Vendée Globe ?",
-                "answers": [
-                    { "title": "La gestion de l'approvisionnement en nourriture", "isCorrect": false },
-                    { "title": "La gestion de la solitude et de l'isolement", "isCorrect": true },
-                    { "title": "La navigation à travers des eaux peu profondes", "isCorrect": false },
-                    { "title": "La prise de décisions rapides pour éviter des obstacles", "isCorrect": false }
-                ],
-                "resultMessage": {
-                    "success": "Bonne réponse ! Le passage près du point Nemo représente un défi psychologique majeur en raison de l'isolement total des marins.",
-                    "failure": "Dommage, ce n'est pas la bonne réponse. La gestion de la solitude et de l'isolement est un véritable défi pour les skippers près du point Nemo."
-                }
+export default function PageHome() {
+   const sections = [
+      {
+         id: 'etape1',
+         title: 'Étape 1: Golfe de Gascogne',
+         content: 'Les skippers affrontent des vagues et des vents violents dans cette première étape.',
+         image: 'src/assets/golfe_de_gascone.png',
+         trait: 'src/assets/trait/1.png',
+         globe: 'src/assets/globe/Golfe_de_gascogne.png',
+      },
+      {
+         id: 'etape2',
+         title: 'Étape 2: Anticyclone des Açores',
+         content: 'Les marins traversent une zone de calme, où il n’y a pas de vent.',
+         image: 'src/assets/anticyclone_des_acores.png',
+         trait: 'src/assets/trait/2.png',
+         globe: 'src/assets/globe/anticyclone_des_acores.png',
+      },
+      {
+         id: 'etape3',
+         title: 'Étape 3: Pot au Noir',
+         content: 'Un phénomène météorologique chaud et humide qui affecte la visibilité et la navigation.',
+         image: 'src/assets/pot_au_noir.png',
+         trait: 'src/assets/trait/3.png',
+         globe: 'src/assets/globe/pot_au_noir.png',
+      },
+      {
+         id: 'etape4',
+         title: 'Étape 4: Anticyclone de Saint-Hélène',
+         content: 'Zone de haute pression avec de beaux temps, mais difficile à contourner.',
+         image: 'src/assets/anticyclone_de_sainte-helene.png',
+         trait: 'src/assets/trait/4.png',
+         globe: 'src/assets/globe/anticyclone_de_sainte_helene.png',
+      },
+      {
+         id: 'etape5',
+         title: 'Étape 5: Cap de Bonne-Espérance',
+         content: 'Les marins traversent l’Atlantique Sud et se dirigent vers les mers australes.',
+         image: 'src/assets/cap_de_bonne-esperance.png',
+         trait: 'src/assets/trait/5.png',
+         globe: 'src/assets/globe/cap_de_bonne_esperance.png',
+      },
+      {
+         id: 'etape6',
+         title: 'Étape 6: Cap Leeuwin',
+         content: 'Les skippers passent près de l’Australie, entrée dans l’océan Pacifique.',
+         image: 'src/assets/cap_leeuwin.png',
+         trait: 'src/assets/trait/6.png',
+         globe: 'src/assets/globe/cap_leeuwin.png',
+      },
+      {
+         id: 'etape7',
+         title: 'Étape 7: Point Nemo',
+         content: 'Le point le plus éloigné de toute terre émergée, au cœur du Pacifique Sud.',
+         image: 'src/assets/point_nemo.png',
+         trait: 'src/assets/trait/7.png',
+         globe: 'src/assets/globe/point_nemo.png',
+         quiz: {
+            "question": "Quel est l'un des défis majeurs pour les skippers lors du passage près du point Nemo pendant le Vendée Globe ?",
+            "answers": [
+                { "title": "La gestion de l'approvisionnement en nourriture", "isCorrect": false },
+                { "title": "La gestion de la solitude et de l'isolement", "isCorrect": true },
+                { "title": "La navigation à travers des eaux peu profondes", "isCorrect": false },
+                { "title": "La prise de décisions rapides pour éviter des obstacles", "isCorrect": false }
+            ],
+            "resultMessage": {
+                "success": "Bonne réponse ! Le passage près du point Nemo représente un défi psychologique majeur en raison de l'isolement total des marins.",
+                "failure": "Dommage, ce n'est pas la bonne réponse. La gestion de la solitude et de l'isolement est un véritable défi pour les skippers près du point Nemo."
             }
-        },
-        { id: 'etape8', title: 'Étape 8: Cap Horn', content: 'Un passage difficile où les conditions climatiques peuvent être extrêmes.' },
-        { id: 'finale', title: 'Finale: Retour aux Sables-d’Olonne', content: 'Le retour triomphal après avoir traversé l’océan Atlantique et le Vendée Globe.' },
-    ];    
+        }
+      },
+      {
+         id: 'etape8',
+         title: 'Étape 8: Cap Horn',
+         content: 'Un passage difficile où les conditions climatiques peuvent être extrêmes.',
+         image: 'src/assets/cap_horn.png',
+         trait: 'src/assets/trait/8.png',
+         globe: 'src/assets/globe/cap_horn.png',
+      },
+      {
+         id: 'finale',
+         title: 'Finale: Retour aux Sables-d’Olonne',
+         content: 'Le retour triomphal après avoir traversé l’océan Atlantique et le Vendée Globe.',
+         image: 'src/assets/sable_olonne.png',
+         trait: 'src/assets/trait/9.png',
+         globe: 'src/assets/globe/sable_olonne.png',
+      },
+   ];
 
-    return (
-    <div>                
-        {sections.map((section) => (
+   return (
+      <div>
+         {sections.map((section) => (
             <Element key={section.id} name={section.id}>
-                <Section title={section.title} content={section.content} quiz={section.quiz} />
+               <Section title={section.title} content={section.content} image={section.image} trait={section.trait} globe={section.globe} quiz={section.quiz} />
             </Element>
-        ))}
-    </div>
-  )
+         ))}
+      </div>
+   );
 }
