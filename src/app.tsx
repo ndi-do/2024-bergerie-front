81df6fb93178862c38d3862e206a26103c9beb4f
Sarah <sarah.theoulle@etu.umontpellier.fr>
2024-12-05 21:23:15 +0100

    feat(home) : add homepage component

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
 import PageHome from '@/pages/home'
 import {  Routes, Route } from 'react-router-dom';
-import Navbar from "@/components/navbar.tsx";
+import Accueil from './pages/accueil';
+// import CarteInteractive from "@/pages/carte.tsx";
 
 export default function App() {
 
     return (
         <>
             <div>
-                <Navbar />
+                <Accueil/>
+
                 <Routes>
                     <Route path="/" element={<PageHome />} />
                 </Routes>
