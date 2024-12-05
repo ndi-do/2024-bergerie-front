df12ab8a3a8cf75f13d45108ffe77e2f77510d13
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-05 20:23:06 +0100

    feat: create navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,19 @@
 import PageHome from '@/pages/home'
 import {  Routes, Route } from 'react-router-dom';
 import CarteInteractive from "@/pages/carte.tsx";
+import Navbar from "@/components/navbar.tsx";
 
 export default function App() {
 
     return (
         <>
             <div>
-                    <div className="App">
-                        <Routes>
-                            <Route path="/" element={<PageHome />} />
-                            <Route path="/carte" element={<CarteInteractive />} />
-                            {/* Ajoutez d'autres routes ici */}
-                        </Routes>
-                    </div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<PageHome />} />
+                    <Route path="/carte" element={<CarteInteractive />} />
+                    {/* Ajoutez d'autres routes ici */}
+                </Routes>
             </div>
         </>
     )
