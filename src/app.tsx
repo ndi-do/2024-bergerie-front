509897a7fc0a320b899c3d439b9d048790e49a88
pontatot <hugo.du-peloux@etu.umontpellier.fr>
2024-12-06 07:30:40 +0100

    feat: add podcast page and update navbar (#31)

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import PageHome from '@/pages/home'
 import {  Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import CreditPage from './components/credits/credit-page';
+import PagePodcast from './pages/podcast';
 // import CarteInteractive from "@/pages/carte.tsx";
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<PageHome />} />
                     <Route path="/credits" element={<CreditPage />} />
+                    <Route path="/podcast" element={<PagePodcast />} />
                     
                 </Routes>
             </div>
