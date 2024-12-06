636fdc323a3b3b8cab96702b99993fa5cff91a7f
hugo.du-peloux <hugo.du-peloux@etu.umontpellier.fr>
2024-12-06 06:04:35 +0100

    feat: closing screen

---
+++ src/games/closing-screen.tsx
@@ -0,0 +1,11 @@
+// src/components/ClosingScreen.tsx
+
+const ClosingScreen = () => {
+  return (
+    <div className="closing-screen">
+      <h1>Thank you for playing!</h1>
+    </div>
+  );
+};
+
+export default ClosingScreen;
