df24481e58621970415a50951f7fad073fcfcba1
Sarah THEOULLE <106761192+stheoulle@users.noreply.github.com>
2024-12-06 02:26:22 +0100

    Feat/defense des recifs (#15)
    * feat: init game `defense-des-recifs`
    
    * feat(relais des courants) : add grid
    
    * feat(relais des océans) : add colors and verif
    
    * pnpm update
    
    ---------
    
    Co-authored-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,18 @@ import PageHome from '@/pages/home'
 import {  Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Accueil from './pages/accueil';
+import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
+import Popup from "@/components/popup";
+import DefenseDesRecifs from "@/games/defense-des-recifs.tsx";
+import RelaisDesCourants from './games/relais-des-courants';
 // import CarteInteractive from "@/pages/carte.tsx";
 
 export default function App() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpenCourants, setIsModalOpenCourants] = useState(false);
+
+
 
     return (
         <>
@@ -12,6 +21,22 @@ export default function App() {
             <Navbar />
 
                 <Accueil/>
+
+                <Button onClick={() => setIsModalOpen(true)}>
+                    Defense des recifs
+                </Button>
+
+                <Button onClick={() => setIsModalOpenCourants(true)}>
+                    Relais des courants
+                </Button>
+
+                <Popup isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                    <DefenseDesRecifs />
+                </Popup>
+
+                <Popup isOpen={isModalOpenCourants} onClose={() => setIsModalOpenCourants(false)}>
+                    <RelaisDesCourants />
+                </Popup>
 
                 <Routes>
                     <Route path="/" element={<PageHome />} />
