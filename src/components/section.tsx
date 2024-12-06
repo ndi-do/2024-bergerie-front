d99730cfcd3ac853a4ee69858076274ddadd210c
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-12-06 01:03:49 +0100

    implements quiz component

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,12 +1,22 @@
 import { motion } from 'framer-motion';
 import './Section.css';
+import { useState } from 'react';
+import Popup from './popup';
+import QuizForm from './quiz-form';
 
 export interface SectionProps {
     title: string;
     content: string;
+    quiz?: {
+        question: string;
+        answers: { title: string, isCorrect: boolean }[];
+        resultMessage: { success: string, failure: string };
+    }
 }
 
-export default function Section({ title, content }: SectionProps) {
+export default function Section({ title, content, quiz }: SectionProps) {
+    const [showQuestions, setShowQuestions] = useState(false);
+
     return (
         <motion.div
             className="section"
@@ -17,6 +27,24 @@ export default function Section({ title, content }: SectionProps) {
         >
             <h2>{title}</h2>
             <p>{content}</p>
+            {
+                quiz && (
+                    <>
+                    <button
+                        onClick={() => setShowQuestions(true)}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+                    >Question</button>
+
+                    <Popup isOpen={showQuestions} onClose={() => setShowQuestions(false)}>
+                        <QuizForm
+                            question={quiz.question}
+                            answers={quiz.answers}
+                            resultMessage={quiz.resultMessage}
+                        />
+                    </Popup>
+                    </>
+                )
+            }
         </motion.div>
     );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,21 @@ export default function PageHome () {
         { id: 'etape4', title: 'Étape 4: Anticyclone de Saint-Hélène', content: 'Zone de haute pression avec de beaux temps, mais difficile à contourner.' },
         { id: 'etape5', title: 'Étape 5: Cap de Bonne-Espérance', content: 'Les marins traversent l’Atlantique Sud et se dirigent vers les mers australes.' },
         { id: 'etape6', title: 'Étape 6: Cap Leeuwin', content: 'Les skippers passent près de l’Australie, entrée dans l’océan Pacifique.' },
-        { id: 'etape7', title: 'Étape 7: Point Nemo', content: 'Le point le plus éloigné de toute terre émergée, au cœur du Pacifique Sud.' },
+        { id: 'etape7', title: 'Étape 7: Point Nemo', content: 'Le point le plus éloigné de toute terre émergée, au cœur du Pacifique Sud.', 
+            quiz: {
+                "question": "Quel est l'un des défis majeurs pour les skippers lors du passage près du point Nemo pendant le Vendée Globe ?",
+                "answers": [
+                    { "title": "La gestion de l'approvisionnement en nourriture", "isCorrect": false },
+                    { "title": "La gestion de la solitude et de l'isolement", "isCorrect": true },
+                    { "title": "La navigation à travers des eaux peu profondes", "isCorrect": false },
+                    { "title": "La prise de décisions rapides pour éviter des obstacles", "isCorrect": false }
+                ],
+                "resultMessage": {
+                    "success": "Bonne réponse ! Le passage près du point Nemo représente un défi psychologique majeur en raison de l'isolement total des marins.",
+                    "failure": "Dommage, ce n'est pas la bonne réponse. La gestion de la solitude et de l'isolement est un véritable défi pour les skippers près du point Nemo."
+                }
+            }
+        },
         { id: 'etape8', title: 'Étape 8: Cap Horn', content: 'Un passage difficile où les conditions climatiques peuvent être extrêmes.' },
         { id: 'finale', title: 'Finale: Retour aux Sables-d’Olonne', content: 'Le retour triomphal après avoir traversé l’océan Atlantique et le Vendée Globe.' },
     ];    
@@ -18,7 +32,7 @@ export default function PageHome () {
     <div>                
         {sections.map((section) => (
             <Element key={section.id} name={section.id}>
-                <Section title={section.title} content={section.content} />
+                <Section title={section.title} content={section.content} quiz={section.quiz} />
             </Element>
         ))}
     </div>
