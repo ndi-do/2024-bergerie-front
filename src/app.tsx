9bf2bb34d0a9bd936d2aee10c7d5fc43188f1adf
Sarah THEOULLE <106761192+stheoulle@users.noreply.github.com>
2024-12-06 03:50:22 +0100

    feat(game4): add game 4 (#18)

--- src/app.tsx
+++ src/app.tsx
@@ -8,12 +8,15 @@ import Popup from "@/components/popup";
 import DefenseDesRecifs from "@/games/defense-des-recifs.tsx";
 import RelaisDesCourants from './games/relais-des-courants';
 import Plancton from './games/collecte-plancton';
+import CoralBuilder from './games/coral-builder';
 // import CarteInteractive from "@/pages/carte.tsx";
 
 export default function App() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isModalOpenCourants, setIsModalOpenCourants] = useState(false);
     const [isModalOpenPlancton, setIsModalOpenPlancton] = useState(false);
+    const [isModalOpenCoral, setIsModalOpenCoral] = useState(false);
+
 
 
 
@@ -36,6 +39,10 @@ export default function App() {
                     Collecte de planctons
                 </Button>
 
+                <Button onClick={() => setIsModalOpenCoral(true)}>
+                    Coral Builder
+                </Button>
+
                 <Popup isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
                     <DefenseDesRecifs />
                 </Popup>
@@ -46,6 +53,10 @@ export default function App() {
 
                 <Popup isOpen={isModalOpenPlancton} onClose={() => setIsModalOpenPlancton(false)}>
                     <Plancton />
+                </Popup>
+
+                <Popup isOpen={isModalOpenCoral} onClose={() => setIsModalOpenCoral(false)}>
+                    <CoralBuilder />    
                 </Popup>
 
                 <Routes>
